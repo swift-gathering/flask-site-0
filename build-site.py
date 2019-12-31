8e40f2e97e31b45270eb912f665539ba41e1531d
Yenson Lau <yenson.lau@gmail.com>
2019-12-30 20:47:29 -0500

    Working through some tutorials...

---
+++ build-site.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+import sys
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+freezer = Freezer(app)
+pages = FlatPages(app)
+
+# Homepage
+@app.route('/')
+def index():
+  return render_template('index.html', pages=pages)
+
+# Render FlatPages
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+
+if __name__ == '__main__':
+  if (len(sys.argv)>1) and (sys.argv[1] == 'live'):
+    app.run(debug=True)
+  else:
+    freezer.freeze()
